7856eb0a04d22cceed1b17f77dae8078cd337358
Yong Jin Lee <yjinlee@kakao.com>
2020-01-19 20:20:31 +0800

    Add delete functions

--- Diary.sh
+++ Diary.sh
@@ -3,42 +3,62 @@
 echo -e "Hi, welcome to bashDiaries. \nChoose 1 to Write, 2 to Modify or 3 to Delete"
 read action
 
+#If $action equals 1, then the diary creates a new entry.
 if [ $action = "1" ]
 then
 
 	cp template.txt currentDiary.txt
-
 	vim currentDiary.txt
 
-	title=$(grep -i "title:" currentDiary.txt | sed -n '1 p' | awk 'NR=1 {print $2}')
+	#Fetch the title from the saved file, and stores it in $title.
+	#sed first removes the Title: tag, and removes possible blank at the very
+	#beginning of the line.
+	#tr replaces blanks with the underscore.
+	title=$(grep -i "title:" currentDiary.txt | sed -e 's/Title://g' | sed 's/^[ ]//' | tr ' ' _)
 	echo $title
 
+	#Stores the temporarily created file's name
 	originalName=$(ls -lt | sed -n '2 p' | awk 'NR=1 {print $9}')
 	echo $originalName
 
+	#Replaces the temporarily created file's name with the new time and title
+	#information.
 	newName=$(ls -ltT | sed -n '2 p' | awk 'NR==1 {print $9"_"$6"_"$7"_"$8}')
 	newName+="_${title}.txt"
+	
 	echo $newName
 
+	#Changes the name of the file
 	mv $originalName $newName
+
+#If $action equals 2, then the diary modifies an already existing entry.
 elif [ $action = "2" ]
 then
+	#The following lines of code lists the currently saved entries.
 	echo "Following is the files currently registered"
 	ls -Ut | egrep "^[0-9]{4}" 	
 	
+	#Takes in user input to choose the file to modify.
 	echo "Please choose the file that you want to MODIFY."
 	read fileSelection
-	
+
+	#Checks if the desired file is in the list of entries.	
 	fileCheck=$(ls -tU | grep $fileSelection | head -n 1)
 
-	if [ -n $fileCheck ]
+	#If there is a file with the desired name, it opens the file for modification.
+	#If condition checks if the variable, which stores the result of the search,
+	#is empty.
+	if [ ! -z "$fileCheck" ]
 	then
+		echo $fileCheck
 		vim $fileCheck
 	else
-		echo "Notthing"
+		echo "We could not find the file you were requesting"
 	fi
 
-else
+#If $action is 3, then the diary deletes an already existing entry.
+elif [ $action = "3" ]
+then
 	echo "Following is the files currently registered"
 	ls -Ut | egrep "^[0-9]{4}" 	
 	
@@ -47,10 +67,26 @@ else
 	
 	fileCheck=$(ls -tU | grep $fileSelection | head -n 1)
 
-	if [ -n $fileCheck ]
+	if [ ! -z $fileCheck ]
 	then
-		rm $fileCheck
+		echo "We have found the following file:"
+		echo $fileCheck
+		echo -e "\nIf you want to delete the file type yes"
+		read yon
+		
+		if [ "$yon" = "yes" ] 
+		then
+			rm $fileCheck
+			echo "You have deleted the file"
+		else
+			echo $yon
+			echo "You chose not to delete the file"
+		fi
 	else
-		echo "Notthing"
+		echo "We could not find the file you were requesting"
 	fi
+
+else
+	echo "You have selected an action that does not exist"
+	echo "The program will terminate. Thank You."
 fi
